878e6e61c328b5218534fadad35bb63466e9463b
TibebeJs <Tibebes.js@gmail.com>
2021-03-30 15:17:16 +0300

    initial implementation for telegram upload

---
+++ pkg/backup/telegram.go
@@ -0,0 +1,68 @@
+package backup
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+
+	"github.com/stefanprodan/mgob/pkg/config"
+)
+
+func telegramUpload(filename string, plan config.Plan) (string, error) {
+	t1 := time.Now()
+	
+		file, err := os.Open(filename)
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+	
+		url := "https://api.telegram.org/bot"+plan.Token+"/sendDocument?caption="+filename+"&chat_id="+plan.Channel
+	
+		body := &bytes.Buffer{}
+		writer := multipart.NewWriter(body)
+		part, err := writer.CreateFormFile(filename, filepath.Base(file.Name()))
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		io.Copy(part, file)
+		writer.Close()
+		request, err := http.NewRequest("POST", , body)
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		request.Header.Add("Content-Type", writer.FormDataContentType())
+		client := &http.Client{}
+	
+		response, err := client.Do(request)
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer response.Body.Close()
+	
+		content, err := ioutil.ReadAll(response.Body)
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+
+	t2 := time.Now()
+	msg := fmt.Sprintf("Telegram upload finished `%v` -> `%v` Duration: %v",
+		filename, "Chat ID: " + plan.channel, t2.Sub(t1))
+	return msg, nil
+}
